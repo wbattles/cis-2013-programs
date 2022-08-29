3f8cc9fcd68e04aeca4ed63a2d253583d2ec2ef6
wileybattles <wileybattles@gmail.com>
2022-08-29 14:53:13 -0500

    Test

--- my.js
+++ my.js
@@ -13,5 +13,5 @@ var mainProgram = function () {
 
 window.onload = function () {
     $("click").onclick = mainProgram;
-    $("item_cost").focus(); //puts the cursor on the first DOM text input box
+    $("item_cost").focus(); //puts thnput box
 }
