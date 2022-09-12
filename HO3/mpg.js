714eda1aa4abfba7e0e3cd0dca071c9bdef9ea65
wileybattles <wileybattles@gmail.com>
2022-09-12 13:48:05 -0500

    gas1

---
+++ HO3/mpg.js
@@ -0,0 +1,10 @@
+var floatMiles = parse.Float(prompt('Enter miles driven'));
+if (floatMiles == null) {
+    alert('Canceled')
+}
+
+var floatGas = parse.Float(prompt('Enter gallons of gas used'));
+if (floatMiles == null) {
+    alert('Canceled')
+}
+
