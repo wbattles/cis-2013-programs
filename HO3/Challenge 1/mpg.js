5417598a8ea411485e56a9322608287a5646f2d7
wileybattles <wileybattles@gmail.com>
2022-09-23 21:21:32 -0500

    divide challenges

--- HO3/Challenge 1/mpg.js
+++ HO3/Challenge 1/mpg.js
@@ -1,15 +1,17 @@
+var intMiles, floatGas, floatMpg
+
 //input miles, force type integer
-var intMiles = parseInt(prompt('Enter miles driven'));
+intMiles = parseInt(prompt('Enter miles driven'));
 if (intMiles == null) {
     alert('Canceled');
 }
 
 //input gallons, force type float
-var floatGas = parseFloat(prompt('Enter gallons of gas used'));
+floatGas = parseFloat(prompt('Enter gallons of gas used'));
 if (floatMiles == null) {
     alert('Canceled');
 }
 
 //calculate mpg, display output
-var floatMpg = parseFloat(floatMiles / floatGas);
+floatMpg = parseFloat(floatMiles / floatGas);
 alert('Your car has: ' + floatMpg + 'mpg');
