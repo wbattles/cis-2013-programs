7f1ec6d6a439a1e7b68f454f98ebb9960a16c437
wileybattles <wileybattles@gmail.com>
2022-11-11 13:20:31 -0600

    comments

--- HO8/guess.js
+++ HO8/guess.js
@@ -1,28 +1,35 @@
 var intMax, intMin, intRandom, intGuess, intCount
 
 
+// user input for lowest number of guesing range, checks value
 intMin = parseInt(prompt("Enter lowest number for the guessing game (no less than 0)"));
 while (isNaN(intMin) || intMin <= 0) {
     intMin = parseInt(prompt("Invalid number, try again (no less than 0)"))
 }
 
+// use input for highest number of guessing range, checks value
 intMax = parseInt(prompt("Enter highest number for the guessing game (at least 2 more than 0)"));
 while (isNaN(intMax) || intMax < 2 || intMax < intMin) {
     intMax = parseInt(prompt("Invalid number, try again (at least 2 more than 0)"));
 }
 
 
-//var guessing = function (intMin, intMax) {
+// initialize count, generate random number within range
 intCount = 0
 intRandom = parseInt(Math.floor(Math.random() * (intMax - intMin + 1)) + intMin);
+
+// loop until user enters correct value, count attempts
 while (intGuess !== intRandom) {
     intGuess = parseInt(prompt("Guess the number"));
     intCount++
+    // success message
     if (intGuess === intRandom) {
         alert("Correct")
         break;
-    } else if (intGuess < intMin || intGuess > intMax) {
+    // compares user input to entered range
+    } else if (isNan(intGuess) || intGuess < intMin || intGuess > intMax) {
         alert("Outside of range")
+    // compare user input to correct value
     } else {
         if (intGuess > intRandom) {
             alert("Too high")
@@ -32,7 +39,5 @@ while (intGuess !== intRandom) {
     }
 
 }
-alert("It took you " + intCount + " trys to guess the number")
-
-//}
-
+// message, guess count
+alert("It took you " + intCount + " trys to guess the number");
