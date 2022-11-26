eb8504376d16eac78a18897b5910414ea8686f9b
wbattles <wileybattles@gmail.com>
2022-11-26 15:10:00 -0600

    HO9.2

--- HO9/fib.js
+++ HO9/fib.js
@@ -7,25 +7,28 @@ var $ = function (id) {
 var generate = function () {
     var intCount = parseInt($("total_fib").value);
     //validate user input, break with labled if statement
-    valueCheck: if (isNaN(intCount) || intCount < 2 || intCount > 100) {
+    valueCheck: if (isNaN(intCount) || intCount <= 1 || intCount >= 100) {
         alert("Please enter a number between 1-100")
+        //sets the DOM values back
         $("total_fib").value = ""
         $("output").value = ""
         break valueCheck;
     } else {
         var i = 0;  // sets the first number in the Fibonacci series to 0
-        var j = 1;  //sets the second number in the Fibonacci series to 1
+        var j = 1;  // sets the second number in the Fibonacci series to 1
         var k;      // k is calculated by adding i+j and represents the next Fibonacci number
-        var stringOutput = [0, " 1"];// initialize the Fibonacci series output to include the first two numbers
+        var stringOutput = [0, " 1"];// initialize array to include the first two numbers
 
         // calculates given Fibonacci numbers
         while (intCount > 2) {
             k = i + j
             i = j
             j = k
+            // add number to array
             stringOutput.push(" " + k)
             intCount--
         }
+        //sends output to html
         $("output").value = stringOutput;
     }
 
